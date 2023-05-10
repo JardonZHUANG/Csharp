8c801f4e334463661762174ce9e030a300b818b6
JardonZHUANG <107563312+JardonZHUANG@users.noreply.github.com>
2023-05-10 21:34:52 +0800

    Add files via upload
    项目1

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//   命名空间
+namespace ConsoleApplication1
+{
+    //类：
+    class Program
+    {
+        static void Main( string[ ] args )
+        {
+            Console.WriteLine("hello world");
+             // 字符型
+            char c = 'a';  //ASCII码
+            
+            //字符可以和整型相互转换
+            int code = c; //断点
+            int i = 0;
+
+            //布尔型，只有两个值： true、 false
+            bool ok = true;
+            ok = false;
+
+            //日期型
+            DateTime birthday = new DateTime(1999, 1, 1, 16, 23, 45);
+        }
+    }
+}
